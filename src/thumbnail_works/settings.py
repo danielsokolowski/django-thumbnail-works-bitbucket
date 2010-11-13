8687646d5610d4685f69e63706550028164b4d96
gnotaras <gnotaras@localhost>
2010-11-14 01:38:42 +0200

    Added comments to each setting

--- src/thumbnail_works/settings.py
+++ src/thumbnail_works/settings.py
@@ -28,14 +28,15 @@
 from django.conf import settings
 
 
-# JPEG, PNG
+# Anything supported by PIL
 THUMBNAILS_FORMAT = getattr(settings, 'DTW_THUMBNAILS_FORMAT', 'JPEG')
 
 # For JPEG format only
 THUMBNAILS_QUALITY = getattr(settings, 'DTW_THUMBNAILS_QUALITY', 85)
 
-
 # This is the name of the directory where the thumbnails will be stored
 THUMBNAILS_DIRNAME = getattr(settings, 'DTW_THUMBNAILS_DIRNAME', 'thumbs')
 
+# Generate the thumbnails on first access rather than at the time the
+# original image is saved. 
 THUMBNAILS_DELAYED_GENERATION = getattr(settings, 'DTW_THUMBNAILS_DELAYED_GENERATION', True)
